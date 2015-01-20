d887e5e81d6470fe981b05f878adfde320720295
kicco73 <specifico73@yahoo.it>
2015-01-20 15:49:31 +0100

    Profiling attributes added (ms)

--- src/main/java/it/cnr/iit/retrail/client/PEPProtocol.java
+++ src/main/java/it/cnr/iit/retrail/client/PEPProtocol.java
@@ -2,12 +2,13 @@
  * CNR - IIT
  * Coded by: 2014 Enrico "KMcC;) Carniani
  */
-
 package it.cnr.iit.retrail.client;
 
 import org.w3c.dom.Node;
 
 public interface PEPProtocol {
+
     Node revokeAccess(Node pepSession) throws Exception;
+
     Node runObligations(Node pepSession) throws Exception;
 }
